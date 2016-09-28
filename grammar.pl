9b1ba0c7300a11662394a052d63dffa52e5f7fdf
Martin Persson <martip@kth.se>
2016-09-28 15:17:45 +0200

    Added grammars for addi,sub and subi

--- grammar.pl
+++ grammar.pl
@@ -4,8 +4,15 @@ grammar(Ls,O):- grammar(Ls,[],O).
 grammar([],O,O).
 grammar([H|T],A,O):-statement(H,H1),grammar(T,[H1|A],O).
 
-statement([(ord,add),D,S1,S2],[(ord,add),D1,S11,S21]):-destinationTmp(D,D1),sourceTmp(S1,S11),sourceTmp(S2,S21).
+statement([(ord,C),D,S1,S2],[(ord,C),D1,S11,S21]):-reg_command(C),destinationTmp(D,D1),sourceTmp(S1,S11),sourceTmp(S2,S21).
+statement([(ord,C),D,S,I],[(ord,C),D1,S1,I1]):-imm_command(C),destinationTmp(D,D1),sourceTmp(S,S1),immidiate(I,I1).
 
 
+reg_command(add).
+reg_command(sub).
+imm_command(subi).
+imm_command(addi).
+
+immidiate((ord,X),(imm,X)).
 sourceTmp((temp,X),(sourcetemp,X)).
 destinationTmp((temp,X),(desttemp,X)).
