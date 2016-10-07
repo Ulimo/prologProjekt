adf86e1de262f4210cc3fa2c2f1b84d988fe75c3
Martin Persson <martip@kth.se>
2016-10-07 17:02:21 +0200

    Graph coloring

---
+++ colouring.pl
@@ -0,0 +1,37 @@
+:- module(colouring, [generate/5,colour/3, degreeToList/3]).
+
+colour((Vs,Es),Degree,Output):-
+    degreeToList(Degree,[],RegisterList),
+    generate(Vs,Es,RegisterList,[],Output).
+    %! testColouring(Es,Output).
+
+
+generate([],_,_,A,A).
+generate([HVs|TVs],Es, Colors,A, Output):-
+    member(C,Colors),
+    testNode((HVs,C),Es,A),
+    generate(TVs,Es,Colors,[(HVs,C)|A],Output).
+
+degreeToList(0,A,A) :- !.
+degreeToList(Degree,A,RegisterList):-
+    Next is Degree-1,
+    degreeToList(Next,[Degree|A],RegisterList).
+
+testColouring([],_).
+
+testColouring([((V1),(V2))|Te],ColVs):-
+    member(((V1,_),C1),ColVs),
+    member(((V2,_),C2),ColVs),
+    C1\==C2,
+    testColouring(Te,ColVs).
+
+testNode(_,[],_).
+
+testNode(((V,D),C),[(V,CmpV)|TEs],ColVs):-
+    member(((CmpV,_),C),ColVs),!,fail.
+    %!testNode(((V,D),C),TEs,ColVs).
+testNode(((V,D),C),[(CmpV,V)|TEs],ColVs):-
+    testNode(((V,D),C),[(V,CmpV)|TEs],ColVs).
+
+testNode(V,[_|TEs],ColVs):-
+    testNode(V,TEs,ColVs).
--- main.pl
+++ main.pl
@@ -3,6 +3,7 @@
 :- use_module(liveness).
 :- use_module(ranges).
 :- use_module(graph).
+:- use_module(colouring).
 
 main(O) :-
     readfile('ass.txt', OutputRead),
@@ -10,5 +11,6 @@ main(O) :-
     buildLiveness(OutputGrammar, OutputLiveness),
     buildRanges(OutputLiveness, OutputRanges),
     createGraph(OutputRanges, OutputGraph),
-    removeUnderDegree(OutputGraph, 4, O).
+    removeUnderDegree(OutputGraph, 3, SimplifiedGraph),!,
+    colour(SimplifiedGraph, 3, O).
     %! writeToFile(O).
